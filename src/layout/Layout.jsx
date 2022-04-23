e16d87e35d695c4b82b5aeef03d6ed3b40e05994
luisgonzalesf <luisgonzalesfarronan@gmail.com>
2022-04-23 00:53:56 -0500

    Fix layout.jsx to=clientes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -14,14 +14,16 @@ const Layout = () => {
                     <Link
                         className={`${urlActual === '/clientes' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
                         // className={`${urlActual === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
-                        to="/"
+                        to="/clientes"
+                        // to="/"
                     >
                         Clientes
                     </Link>
                     <Link
                         className={`${urlActual === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
                         // className={`${urlActual === '/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
-                        to="/nuevo"
+                        to="/clientes/nuevo"
+                        // to="/nuevo"
                     >
                         Nuevo cliente
                     </Link>
